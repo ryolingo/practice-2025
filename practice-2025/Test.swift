1efde3265a8c644920c22440944f983cf4125659
ryolingo <ryomatsuya04@gmail.com>
2025-04-21 12:16:15 +0900

    test

---
+++ practice-2025/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  practice-2025
+//
+//  Created by Ryota Matsumoto on 4/21/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Test: View{
+    var body : some View{
+        Text("Hello")
+    }
+}
